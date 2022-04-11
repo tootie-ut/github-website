df8c8d2b05e1254406813a8f825c3f83fa5e14fe
tootie-ut <99643561+tootie-ut@users.noreply.github.com>
2022-04-11 19:05:16 +1000

    more things

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,33 @@
-  mapboxgl.accessToken = 'pk.eyJ1IjoidXRwaWtsZXIiLCJhIjoiY2wwcmM0eWtyMDIwNzNkcXB0bnpneWZnMSJ9.7VV66Ac4S8NH6lXX23I6Ng';
-  var map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/dark-v10'
-  });
+//  mapboxgl.accessToken = 'pk.eyJ1IjoidXRwaWtsZXIiLCJhIjoiY2wwcmM0eWtyMDIwNzNkcXB0bnpneWZnMSJ9.7VV66Ac4S8NH6lXX23I6Ng';
+//  var map = new mapboxgl.Map({
+//      container: 'map',
+//      style: 'mapbox://styles/mapbox/dark-v10',
+//      center: [149.089,-35.340],
+//      zoom:11,
+//  });
 
 
-  //  navigator.geolocation.getCurrentPosition(sucessLocation, errorLocation, {
-  //      enableHighAccuracy: true
-  //  })
-  //
-  //  function successLocation(position) {
-  //      setupMap([position.coords.longitude, position.coords.latitude])
-  //      console.log(position)
-  //      setupMap()
-  //  }
+//  navigator.geolocation.getCurrentPosition(sucessLocation, errorLocation, {
+//      enableHighAccuracy: true
+//  })
+//
+//  function successLocation(position) {
+//      setupMap([position.coords.longitude, position.coords.latitude])
+//      console.log(position)
+//      setupMap()
+//  }
 
-  //  function errorLocation() {
-  //      setupMap([149.128998, -35.282001])
-  }
+//  function errorLocation() {
+//      setupMap([149.128998, -35.282001])
+}
 
-  //default location if error//
+//default location if error//
 
-  //  function setupMap(center) {
-  //      var map = new mapboxgl.Map({
-  //          container: "map",
-  //          style: 'mapbox://styles/mapbox/dark-v10',
-  //          center: center,
-  //          zoom: 15,
+//  function setupMap(center) {
+//      var map = new mapboxgl.Map({
+//          container: "map",
+//          style: 'mapbox://styles/mapbox/dark-v10',
+//          center: center,
+//          zoom: 15,
 
-  //  })
+//  })
